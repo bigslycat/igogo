5f22849a64dc8f330d421c387bae7a510735dc66
Anton Krivokhizhin <akrivokhizhin@akrivokhizhin.local>
2019-01-18 16:21:19 +0300

    fix `Maybe` `or` tests

--- test/maybe/or.js
+++ test/maybe/or.js
@@ -5,8 +5,8 @@ test('returns nothing on the Just instance', t => {
   const just1 = Just(10);
   const just2 = Just(20);
 
-  t.true(just1.or().isNothing);
-  t.true(just1.or(just2).isNothing);
+  t.is(just1.or().getOr('DEFAULT 1'), just1.getOr('DEFAULT 2'));
+  t.is(just1.or(just2).getOr('DEFAULT 1'), just1.getOr('DEFAULT 2'));
 });
 
 test('returns argument on the Nothing instance', t => {
@@ -14,5 +14,5 @@ test('returns argument on the Nothing instance', t => {
   const just = Just(10);
 
   t.true(nothing.or(just).isJust);
-  t.is(nothing.or(just).getOr(), just.getOr());
+  t.is(nothing.or(just).getOr('DEFAULT 1'), just.getOr('DEFAULT 2'));
 });
